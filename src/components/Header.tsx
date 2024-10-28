b98141ba906080f9d45931527df1b5fb9890982c
mstsurnalyakter <surnalyms@gmail.com>
2024-10-28 21:18:15 +0600

    add: image

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+        <div>
+            <img src="../assets/Vector.png" alt="vector" />
+            <h1>FUN88</h1>
+        </div>
+        <div>
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Header
