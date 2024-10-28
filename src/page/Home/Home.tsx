41265ea25e4ec343fcd87554310002cae25d094f
mstsurnalyakter <surnalyms@gmail.com>
2024-10-28 22:33:53 +0600

    add: banner section

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -4,11 +4,21 @@ import Banner from '../../components/Banner'
 
 const Home = () => {
   return (
-    <div className='px-20 space-y-10'>
-        <Header/>
-        <Banner/>
+    <div className="px-20 space-y-10">
+      <Header />
+      <Banner />
+      <div className="flex gap-2 items-center">
+        <img
+          src="https://i.postimg.cc/d0nn2nvP/bell-svgrepo-com-1.png"
+          alt=""
+          className="w-7"
+        />
+        <h2 className="text-2xl text-[#00A6FF]">
+          ¡FELICIDADES artxxxxipa! GANADOR DESTACADO{" "}
+        </h2>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
